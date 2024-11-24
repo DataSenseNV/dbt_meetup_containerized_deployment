4021a3ba18044010982725aeaf13d2e35a8d6e34
Jim Vekemans <jim.vekemans@datasense.be>
2024-11-24 11:04:25 +0100

    Updated customers model

--- models/staging/customers/customers.sql
+++ models/staging/customers/customers.sql
@@ -1,1 +1,31 @@
+{# 
+    This model references a table that is managed by a seed 
+    However, the project might not compile if the seed has not run yet
+    To avoid this, we can:
+    - Check if the table exists, using some of the code we just wrote
+    - If it exists, write the transformation the intented way
+    - If it doesn’t, create a dummy, empty table instead with the exact same column names.
+#}
+
+{%- 
+    set source_relation = adapter.get_relation(
+        database=source(target.dbname, 'raw_customers').database,
+        schema=source(target.dbname, 'raw_customers').schema,
+        identifier=source(target.dbname, 'raw_customers').name
+    )
+-%}
+
+{% set table_exists=source_relation is not none   %}
+
+{% if table_exists %}
+    SELECT 
+        id,
+        name
+    FROM {{ source(target.dbname, 'raw_customers') }}
+{% else %}
+    SELECT 
+        1 AS id,
+        'Jim' AS name
+{% endif %}
+
 select * from {{ source(target.dbname, 'raw_customers') }}
